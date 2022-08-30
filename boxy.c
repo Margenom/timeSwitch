4a8d57a261b9c0b3b74deacd06b6c57d10b8939c
Даниил Швачкин <margenom@yahoo.com>
2022-08-31 00:45:36 +0300

    io.c

--- boxy.c
+++ boxy.c
@@ -6,32 +6,16 @@
 
 /* subs */
 #include "rex.c"
-//#include "io.c"
+#include "io.c"
 #include "time_clock.c"
 
-s7_pointer with_output_to_file_append(s7_scheme *sc, s7_pointer a) {
-	if (!(s7_list_length(sc, a) >1 && s7_is_string(s7_car(a))))
-		return s7_f(sc);
-	s7_pointer port = s7_open_output_file(sc, s7_string(s7_car(a)), "a"),
-		curport = s7_current_output_port(sc);
-	s7_set_current_output_port(sc, port);
-	
-	s7_pointer out = s7_call(sc, s7_cadr(a), s7_nil(sc));
-	//s7_flush_output_port(sc, port);
-	s7_close_output_port(sc, port);
-
-	s7_set_current_output_port(sc, curport);
-	return out;
-}
 
 int main(int argc, char *argv[]) {
 	s7_scheme *sc = s7_init();
 //subs
 	rex(sc);
-//	io(sc);
+	io(sc);
 	time_clock(sc);
-
-	s7_define_function(sc, "with-output-to-file-append", with_output_to_file_append,  2, 0, 0, "(with-output-to-file-append <file> <writer>): output from writer or #f");
 
 	s7_pointer params = s7_nil(sc),
 		data = s7_nil(sc);
--- io.c
+++ io.c
@@ -2,55 +2,21 @@
 #include <stdlib.h>
 #include "s7.h"
 
-s7_pointer io_read_file(s7_scheme *sc, s7_pointer args) {
-	FILE *fp = fopen(s7_string(s7_car(args)), "r");
-	if (!fp) return s7_f(sc);
-
-	fseek(fp, 0L, SEEK_END);
-	long file_size = ftell(fp);
-	fseek(fp, 0L, SEEK_SET);
-
-	char*buf = malloc(file_size);
-	if(0>fread(buf, file_size, 1, fp)) {
-		free(buf);
-		fclose(fp);
+s7_pointer with_output_to_file_append(s7_scheme *sc, s7_pointer a) {
+	if (!(s7_list_length(sc, a) >1 && s7_is_string(s7_car(a))))
 		return s7_f(sc);
-	}
+	s7_pointer port = s7_open_output_file(sc, s7_string(s7_car(a)), "a"),
+		curport = s7_current_output_port(sc);
+	s7_set_current_output_port(sc, port);
+	
+	s7_pointer out = s7_call(sc, s7_cadr(a), s7_nil(sc));
+	//s7_flush_output_port(sc, port);
+	s7_close_output_port(sc, port);
 
-	s7_pointer out = s7_make_string(sc, buf);
-	free(buf);
-	fclose(fp);
+	s7_set_current_output_port(sc, curport);
 	return out;
 }
 
-s7_pointer io_addto_file(s7_scheme *sc, s7_pointer args) {
-	FILE *fp = fopen(s7_string(s7_car(args)), "a");
-	if (!fp) return s7_f(sc);
-
-	s7_pointer str = s7_string(s7_cadr(args));
-	if(0>fwrite(s7_string(str), s7_string_length(str), 1, fp)) {
-		fclose(fp);
-		return s7_f(sc);
-	}
-	fclose(fp);
-	return s7_string_length(str);;
-}
-
-s7_pointer io_write_file(s7_scheme *sc, s7_pointer args) {
-	FILE *fp = fopen(s7_string(s7_car(args)), "w");
-	if (!fp) return s7_f(sc);
-
-	s7_pointer str = s7_string(s7_cadr(args));
-	if(0>fwrite(s7_string(str), s7_string_length(str), 1, fp)) {
-		fclose(fp);
-		return s7_f(sc);
-	}
-	fclose(fp);
-	return s7_string_length(str);
-}
-
 void io(s7_scheme *sc) {
-	s7_define_function(sc, "io-read-file", io_read_file,  1, 0, 0, "(io-read-file file)");
-	s7_define_function(sc, "io-write-file", io_write_file, 2, 0, 0, "(io-write-file file cont)");
-	s7_define_function(sc, "io-append-file", io_addto_file, 2, 0, 0, "(io-append-file file append)");
+	s7_define_function(sc, "with-output-to-file-append", with_output_to_file_append,  2, 0, 0, "(with-output-to-file-append <file> <writer>): output from writer or #f");
 }
